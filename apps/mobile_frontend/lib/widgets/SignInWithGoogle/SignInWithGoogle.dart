706b1bb79f586e369e5b7af99541d8f932e9cd3f
Arunscape <arun@woosaree.xyz>
2019-04-11 20:35:04 -0600

    moar refactoring

--- apps/mobile_frontend/lib/widgets/SignInWithGoogle/SignInWithGoogle.dart
+++ apps/mobile_frontend/lib/widgets/SignInWithGoogle/SignInWithGoogle.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+class SignInWithGoogle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new RaisedButton(
+      child: Text('Login with Google'),
+      onPressed: () {
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(builder: (context) => SecondRoute()),
+        // );
+      },
+    );
+  }
+}
