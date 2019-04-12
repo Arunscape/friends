8edf736059bd9acb785137ba9bca21d6d23f5045
Jacob Reckhard <jacobrec@live.com>
2019-04-12 22:32:46 +0000

    made my scripts better

--- admin/build.sh
+++ admin/build.sh
@@ -7,9 +7,21 @@ function build_all () {
   git checkout $2
   docker build -f $LOC/admin/Dockerfile.api_server -t $1$API_SERVER . --rm
 }
-
-build_all "prod_" master
-build_all "dev_" master
-build_all "spike_" devops
+case "$1" in
+  "")
+    build_all "prod_" devops
+    build_all "dev_" devops
+    build_all "spike_" devops
+    ;;
+  "prod")
+    build_all "prod_" devops
+    ;;
+  "dev")
+    build_all "dev_" devops
+    ;;
+  "spike")
+    build_all "spike_" devops
+    ;;
+esac
 
 echo "Y" | docker system prune
