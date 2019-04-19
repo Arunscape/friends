81ab85cd3ece0e98c14253f743e4965dc371b487
Jacob Reckhard <jacobreckhard@gmail.com>
2019-04-19 16:20:54 -0600

    Made messages go the right direction

--- apps/mobile_frontend/lib/widgets/ChatScreen.dart
+++ apps/mobile_frontend/lib/widgets/ChatScreen.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
-import './Message.dart';
+import 'Message.dart';
 
 class ChatScreen extends StatelessWidget {
+  final List<String> msgs = ["One", "Two", "Three"];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      padding: EdgeInsets.zero,
-      physics: ScrollPhysics(),
-      children: <Widget>[
-        Center(
-          child: ListBody(children: [
-            new Message("Hey look, a text"),
-            new Message("This is a replay"),
-            new Message("And a third message, cause why not."),
-          ]),
-        )
-      ],
+    return Container(
+      height: double.maxFinite,
+      color: Colors.deepOrange,
+      child: ListView.builder(
+        reverse: true,
+        itemBuilder: (context, position) {
+          return Card(
+            child: new Message(msgs[position]),
+          );
+        },
+        itemCount: msgs.length,
+      ),
     );
   }
 }
