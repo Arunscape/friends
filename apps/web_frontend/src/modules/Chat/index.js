82130ec44b02174c0a32fa295cf1d74a3047fbf3
Jacob Reckhard <jacobreckhard@gmail.com>
2019-06-22 13:18:03 -0600

    autoredirect users to log them out

--- apps/web_frontend/src/modules/Chat/index.js
+++ apps/web_frontend/src/modules/Chat/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Redirect } from 'react-router'
 
 import Message from './Message'
 import MessageBar from './SendMessage'
 
+import { isTokenValid } from 'services/security/token'
+
 export default class ChatPage extends React.Component {
   render () {
+    if (!isTokenValid()) {
+      return <Redirect to='/' />
+    }
     return (
       <>
         <Message text={'Hello, World'} />
