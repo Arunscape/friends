5dd089a4e1f4d709823ab5ad7880c0a1879d3cb5
Jacob Reckhard <jacobreckhard@gmail.com>
2019-06-23 17:04:29 -0600

    got bar to bottom

--- apps/web_frontend/src/modules/Chat/SendMessage.jsx
+++ apps/web_frontend/src/modules/Chat/SendMessage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
 import Icon from '@material-ui/core/Icon'
 import IconButton from '@material-ui/core/IconButton'
 import TextField from '@material-ui/core/TextField'
@@ -7,12 +9,18 @@ import TextField from '@material-ui/core/TextField'
 export default class SendMessageBar extends React.Component {
   render () {
     return (
-      <div className='hor'>
-        <TextField />
-        <IconButton>
-          <Icon>send</Icon>
-        </IconButton>
-      </div>
+      <Box position='absolute' bottom='0' width='80%'>
+        <Grid container spacing={2}>
+          <Grid item xs={11}>
+            <TextField fullWidth label='Send Message' />
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton>
+              <Icon>send</Icon>
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Box>
     )
   }
 }
--- apps/web_frontend/src/modules/Chat/index.js
+++ apps/web_frontend/src/modules/Chat/index.js
@@ -7,17 +7,22 @@ import MessageBar from './SendMessage'
 import { isTokenValid } from 'services/security/token'
 import Header from 'components/atoms/Header'
 
+import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+
 export default class ChatPage extends React.Component {
   render () {
     if (!isTokenValid()) {
       return <Redirect to='/' />
     }
     return (
-      <>
+      <Box height='100vh'>
         <Header title='Chat' />
-        <Message text={'Hello, World'} />
-        <MessageBar />
-      </>
+        <Container maxWidth='md' height='100%'>
+          <Message text={'Hello, World'} />
+          <MessageBar />
+        </Container>
+      </Box>
     )
   }
 }
